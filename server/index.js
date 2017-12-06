50d97bce9dcae93469df9df208fa90fcfc0dfb94
Zach Fagerness <zachfagerness@gmail.com>
2017-12-06 10:27:51 -0600

    Added mongo pre sockets

--- server/index.js
+++ server/index.js
@@ -3,31 +3,13 @@ const path = require('path');
 const app = express();
 var mongo = require('mongodb');
 var monk = require('monk');
+
 var db = monk('mongodb://localhost:27017/test');
 var collection = db.get('restaurants');
-    collection.find({},function(e,docs){
-        console.log(docs);
-    })
-//var MongoClient = require('mongodb').MongoClient;
-//var url = "mongodb://localhost:27017/test";
-//
-// var mongoose = require('mongoose');
-//
-// mongoose.connect('mongodb://localhost/db_name');
-// //
-// MongoClient.connect(url, function(err, db) {
-//   if (err) throw err;
-//   console.log(db.collection);
-//
-//   var query = { address: "Park Lane 38" };
-//   //db.collection("restaurants").find(query).toArray(function(err, result) {
-//     ///if (err) throw err;
-//     //console.log(result);
-//     //db.close();
-// //  });
-//
-//
-// });
+collection.find({},function(e,docs){
+    console.log(docs[0]);
+})
+
 
 app.use(express.static('../client/bin'));
 app.use(express.static('../client/css'));
