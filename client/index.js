af90c1e9d1cafe82f50b837e9ad027bc95c3827f
Zach Fagerness <zachfagerness@gmail.com>
2017-12-06 15:23:02 -0600

    Sockets online, create server with http not express

--- client/index.js
+++ client/index.js
@@ -2,6 +2,14 @@ if (process.env.NODE_ENV !== 'production') {console.log('DEVELOPMENT MODE');}
 import React,{Component} from 'react';
 import {render} from 'react-dom';
 import Home from './src/view/home.js';
+import io from 'socket.io-client';
+var socket = io('http://localhost:3000');
+
+socket.on('connect', function(s){
+  // s.on('chat message', function(msg){
+  //   console.log('message: ' + msg);
+  // });
+});
 
 class Root extends Component{
   render(){
--- server/index.js
+++ server/index.js
@@ -1,15 +1,29 @@
+//
+// var app = require('express')();
+// var http = require('http').Server(app);
+// var io = require('socket.io')(http);
+// var port = process.env.PORT || 3000;
+//
+// app.get('/', function(req, res){
+//   res.sendFile(__dirname + '/index.html');
+// });
+//
+// io.on('connection', function(socket){
+//   socket.on('chat message', function(msg){
+//     io.emit('chat message', msg);
+//   });
+// });
+//
+// http.listen(port, function(){
+//   console.log('listening on *:' + port);
+// });
+
 const express = require('express');
 const path = require('path');
 const app = express();
-var mongo = require('mongodb');
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
 var monk = require('monk');
-
-var db = monk('mongodb://localhost:27017/test');
-var collection = db.get('restaurants');
-collection.find({},function(e,docs){
-    console.log(docs[0]);
-})
-
 
 app.use(express.static('../client/bin'));
 app.use(express.static('../client/css'));
@@ -18,4 +32,23 @@ app.get('/', function(req, res) {
 });
 
 
-app.listen(3000, () => console.log('Port 3000'));
+
+
+io.on('connection', function(socket){
+  socket.on('chat message', function(msg){
+    io.emit('chat message', msg);
+  });
+  console.log('a user connected');
+});
+
+
+var db = monk('mongodb://localhost:27017/test');
+var collection = db.get('restaurants');
+collection.find({},function(e,docs){
+    console.log(docs[0]);
+})
+
+
+
+
+http.listen(3000, () => console.log('Port 3000'));
